ecd114e00b0d8d56a9e209045d02f4a029296eee
hosenur <hosenur.dev@gmail.com>
2024-10-01 15:23:48 +0530

    created app with remix

---
+++ apps/web/app/routes/auth.ts
@@ -0,0 +1,29 @@
+import { Form } from "@remix-run/react"
+import { useState } from "react"
+import { Button } from "~/components/ui/button"
+import { authClient } from "~/lib/auth.client"
+
+export default function Signup() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const signIn = async () => {
+        console.log("pressed")
+        await authClient.signIn.social({
+            provider: 'github'
+        })
+ 
+    }
+
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <Form onSubmit={signIn} className="flex flex-col gap-2 w-96">
+                <Button
+                    type="submit"
+                >
+                    Sign In
+                </Button>
+            </Form>
+        </div>
+    )
+}
